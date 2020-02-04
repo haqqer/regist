c5dad6f412335d5b081f3808582bee3280307318
haqqer <ilhamserbaguna@gmail.com>
2020-02-04 16:17:27 +0700

    add feature service and controller (update, delete, findone)

--- controllers/registrant.js
+++ controllers/registrant.js
@@ -5,16 +5,38 @@ const mailer = require('../helpers/mailer')
 
 const router = express.Router()
 
+// Get All Registrant
 router.get('/', async (req, res) => {
     const result = await service.getAll()
-    return await response(res, true, 'success', result, 200)
+    return await response(res, true, 'success get All registrant', result, 200)
 })
 
+// Get One Registrant by ID
+router.get('/:id', async (req, res) => {
+    const result = await service.getOne({ id: req.params.id })
+    return await response(res, true, 'success get one registrant', result, 200)
+})
+
+// Create Registrant
 router.post('/', async (req, res) => {
     req.body.status = 0
     const result = await service.create(req.body)
-    mailer()
-    return await response(res, true, 'success data', result, 201)
+    const sendEmail = await mailer('ahmadhaqqi690@gmail.com', req.body.email, 'Pendaftaran', {
+        name: req.body.name
+    })
+    return await response(res, true, 'success create registrant', result, 201)
+})
+
+// Update Registrant By ID
+router.put('/:id', async (req, res) => {
+    const result = await service.update(req.body, { id: req.params.id })
+    return await response(res, true, 'success create registrant', result, 201)
+})
+
+// Delete Registrant
+router.delete('/:id', async (req, res) => {
+    const result = await service.destroy({ id: req.params.id })
+    return await response(res, true, 'success delete registrant', result, 200)
 })
 
 module.exports = router
--- services/registrant.js
+++ services/registrant.js
@@ -3,7 +3,7 @@ const { Registrant } = require('../models')
 const getAll = async () => {
     const result = await Registrant.findAll()
     if(!result) {
-        throw Error('Failed to get Registrant')
+        throw Error('Failed to get all Registrant')
     }
     return result
 }
@@ -16,7 +16,36 @@ const create = async (data) => {
     return result
 }
 
+const getOne = async (where) => {
+    const result = await Registrant.findOne({
+        where: where
+    })
+    if(!result) {
+        throw Error('Failed to get Registrant')
+    }
+    return result
+}
+
+const update = async (data, where) => {
+    const result = await Registrant.update(data, { where: where })
+    if(!result) {
+        throw Error('Failed to update Registrant')
+    }
+    return result
+}
+
+const destroy = async (where) => {
+    const result = await Registrant.destroy({ where: where })
+    if(!result) {
+        throw Error('Failed to delete Registrant')
+    }
+    return result
+}
+
 module.exports = {
     getAll,
-    create
+    create,
+    getOne,
+    update,
+    destroy
 }
