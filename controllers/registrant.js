e9f15871540b8d0b3d706ed5d4d7e39def97a0e0
haqqer <ilhamserbaguna@gmail.com>
2020-02-05 15:11:28 +0700

    add update status controller

--- controllers/registrant.js
+++ controllers/registrant.js
@@ -9,39 +9,76 @@ const router = express.Router()
 
 // Get All Registrant
 router.get('/', auth.verify, async (req, res) => {
-    const result = await service.getAll()
-    return await response(res, true, 'success get All registrant', result, 200)
+    try {
+        const result = await service.getAll()
+        if(result.length < 1) {
+            return await response(res, true, 'Registrant is empty', {}, 200)
+        }
+        return await response(res, true, 'success get All registrant', result, 200)
+    } catch (error) {
+        return await response(res, false, 'Error', error, 500)
+    }
 })
 
 // Get One Registrant by ID
 router.get('/:id', auth.verify, async (req, res) => {
-    const result = await service.getOne({ id: req.params.id })
-    return await response(res, true, 'success get one registrant', result, 200)
+    try {
+        const result = await service.getOne({ id: req.params.id })
+        return await response(res, true, 'success get one registrant', result, 200)
+    } catch (error) {
+        return await response(res, false, 'Error', error, 500)
+    }
 })
 
 // Create Registrant
 router.post('/', async (req, res) => {
-    req.body.status = 0
-    const result = await service.create(req.body)
-    const qrcodeImage = await qrcode.toDataURL(req.body.email, { width: 300 })
-    const sendEmail = await mailer('ahmadhaqqi690@gmail.com', req.body.email, 'Pendaftaran Event', {
-        name: req.body.name,
-        email: req.body.email,
-        qrcode: qrcodeImage
-    })
-    return await response(res, true, 'success create registrant', result, 201)
+    try {
+        req.body.status = 0
+        const result = await service.create(req.body)
+        const qrcodeImage = await qrcode.toDataURL(req.body.email, { width: 300 })
+        const sendEmail = await mailer('ahmadhaqqi690@gmail.com', req.body.email, 'Pendaftaran Event', {
+            name: req.body.name,
+            email: req.body.email,
+            qrcode: qrcodeImage
+        })
+        return await response(res, true, 'success create registrant', result, 201)
+    } catch (error) {
+        return await response(res, false, 'Error', error, 500)
+    }
 })
 
 // Update Registrant By ID
 router.put('/:id', async (req, res) => {
-    const result = await service.update(req.body, { id: req.params.id })
-    return await response(res, true, 'success create registrant', result, 201)
+    try {
+        const result = await service.update(req.body, { id: req.params.id })
+        return await response(res, true, 'success create registrant', result, 201)
+    } catch (error) {
+        return await response(res, false, 'Error', error, 500)
+    }
 })
 
 // Delete Registrant
 router.delete('/:id', async (req, res) => {
-    const result = await service.destroy({ id: req.params.id })
-    return await response(res, true, 'success delete registrant', result, 200)
+    try {
+        const result = await service.destroy({ id: req.params.id })
+        return await response(res, true, 'success delete registrant', result, 200)
+    } catch (error) {
+        return await response(res, false, 'Error', error, 500)
+    }
 })
 
+router.put('/status/:regisId', async (req, res) => {
+    try {
+        console.log(req.params.regisId)
+        console.log(req.body)
+        const result = await service.update({ status: req.body.status }, { regisId: req.params.regisId })
+        if(result[0] == 0) {
+            return await response(res, true, 'success update status registrant', { 'message': 'registrant not found!' }, 404)    
+        }
+        return await response(res, true, 'success update status registrant', result, 200)
+    } catch (error) {
+        console.log(error)
+        return await response(res, false, 'Error', error, 500)
+    }
+})
 module.exports = router
